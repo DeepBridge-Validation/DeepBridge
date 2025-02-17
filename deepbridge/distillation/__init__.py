98de650a1f50c6a3335089aafddfc525c7d62218
Gustavo Haase <gustavo.haase@gmail.com>
2025-02-17 00:45:32 -0300

    path modulos_3

--- deepbridge/distillation/__init__.py
+++
@@ -1,4 +0,0 @@
-from .classification.knowledge_distillation import KnowledgeDistillation
-from .classification.model_registry import ModelType
-
-__all__ = ['KnowledgeDistillation', 'ModelType']
--- deepbridge/distillation/classification/__init__.py
+++
@@ -1,4 +0,0 @@
-from .knowledge_distillation import KnowledgeDistillation
-from .model_registry import ModelType
-
-__all__ = ['KnowledgeDistillation', 'ModelType']
