b076bdbb2e02d6d79415c55dad172413421aa405
Gustavo Haase <gustavo.haase@gmail.com>
2025-02-17 23:44:39 -0300

    versao e dependencias

--- deepbridge/__init__.py
+++ deepbridge/__init__.py
@@ -6,7 +6,7 @@ from deepbridge.db_data import DBDataset
 from deepbridge.experiment import Experiment
 from deepbridge.auto_distiller import AutoDistiller
 
-__version__ = "0.2.0"
+__version__ = "0.1.2"
 __author__ = "Team DeepBridge"
 
 __all__ = [
