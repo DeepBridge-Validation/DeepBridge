52706e10c93e576b2a58cbd104317c546752d9f3
Gustavo Haase <gustavo.haase@gmail.com>
2025-02-21 11:25:51 -0300

    synthetic

--- deepbridge/processing/__init__.py
+++ deepbridge/processing/__init__.py
@@ -6,10 +6,12 @@ from deepbridge.processing.data_validator import DataValidator
 from deepbridge.processing.feature_manager import FeatureManager
 from deepbridge.processing.model_handler import ModelHandler
 from deepbridge.processing.probability_manager import DatabaseProbabilityManager
+from deepbridge.processing.synthetic_data_generator import SyntheticDataGenerator
 
 __all__ = [
     "DataValidator",
     "FeatureManager",
     "ModelHandler",
-    "DatabaseProbabilityManager"
+    "DatabaseProbabilityManager",
+    "SyntheticDataGenerator"
 ]
