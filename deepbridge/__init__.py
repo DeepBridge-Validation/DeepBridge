65592a058b12735bee514c40d9dfa006e8c8826d
Gustavo Haase <gustavo.haase@gmail.com>
2025-04-11 07:42:03 -0300

    pygam

--- deepbridge/__init__.py
+++ deepbridge/__init__.py
@@ -6,7 +6,7 @@ and performance analysis to create efficient machine learning models.
 """
 
 # Version information
-__version__ = "0.1.29"
+__version__ = "0.1.30"
 __author__ = "Team DeepBridge"
 
 # Core components
